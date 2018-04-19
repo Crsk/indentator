f06c8e58f33f332a511197f8812775890f35e88a
Christopher Kiessling <crsk@mail.com>
2018-04-19 19:44:45 -0300

    indentator code

---
+++ spec/indentator-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import IndentatorView from '../lib/indentator-view';
+
+describe('IndentatorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
